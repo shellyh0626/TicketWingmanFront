95fff6d05bbefb35252f27155b658b49993e7851
9Mohamedismail <87681402+9Mohamedismail@users.noreply.github.com>
2023-07-11 16:56:44 -0400

    setup React Router. Links to Home, Login, Signup, Profile, and Forgot are created.

---
+++ src/pages/Login.jsx
@@ -0,0 +1,5 @@
+function Login() {
+  return <h1>Welcome to Login</h1>;
+}
+
+export default Login;
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,5 @@
+function Profile() {
+  return <h1>Welcome to Profile</h1>;
+}
+
+export default Profile;
