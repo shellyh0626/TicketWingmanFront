2cd2d190c3486edaed458790892d196f9906b74f
Kaifeng <1536771545kai@gmail.com>
2023-07-14 15:15:13 -0400

    Add action to fetch  weather in Fahrenheit

--- src/redux/weather/weather.actions.js
+++ src/redux/weather/weather.actions.js
@@ -7,3 +7,17 @@ export const FETCH_WEATHER_F = (payload) =>{
         payload:payload
     }
 }
+
+export const FETCH_WEATHER_F_THUNK = (weatherObject) => {
+    return async (dispatch) => {
+        try {
+          console.log("fetch weather data in fahrenheit started");
+          const result = await axios.get("http://localhost:8080/api/weather/displayInFahrenheit",{weatherObject});
+          console.log("fetch weather data in fahrenheit COMPLETED")
+          console.log(result);
+          dispatch(FETCH_WEATHER_F(result));
+        } catch (err) {
+          console.error(err);
+        }
+      };
+}
