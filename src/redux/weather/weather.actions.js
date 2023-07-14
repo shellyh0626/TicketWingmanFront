298941944c0dcc3ff0823febd679438101c7e7b2
Kaifeng <1536771545kai@gmail.com>
2023-07-14 15:28:53 -0400

    add action to fetch weather in Celsius

--- src/redux/weather/weather.actions.js
+++ src/redux/weather/weather.actions.js
@@ -1,23 +1,44 @@
 import axios from "axios";
 import weatherActionTypes from "./weather.types";
 
-export const FETCH_WEATHER_F = (payload) =>{
-    return{
-        type:weatherActionTypes.weatherTempF,
-        payload:payload
+export const FETCH_WEATHER_F = (payload) => {
+    return {
+        type: weatherActionTypes.weatherTempF,
+        payload: payload
+    }
+}
+
+export const FETCH_WEATHER_C = (payload) => {
+    return {
+        type: weatherActionTypes.weatherTempC,
+        payload: payload
     }
 }
 
 export const FETCH_WEATHER_F_THUNK = (weatherObject) => {
     return async (dispatch) => {
         try {
-          console.log("fetch weather data in fahrenheit started");
-          const result = await axios.get("http://localhost:8080/api/weather/displayInFahrenheit",{weatherObject});
-          console.log("fetch weather data in fahrenheit COMPLETED")
-          console.log(result);
-          dispatch(FETCH_WEATHER_F(result));
+            console.log("fetch weather data in Fahrenheit started");
+            const result = await axios.get("http://localhost:8080/api/weather/displayInFahrenheit", { weatherObject });
+            console.log("fetch weather data in Fahrenheit COMPLETED")
+            console.log(result);
+            dispatch(FETCH_WEATHER_F(result));
         } catch (err) {
-          console.error(err);
+            console.error(err);
         }
-      };
+    };
+}
+
+export const FETCH_WEATHER_C_THUNK = (weatherObject) => {
+    return async (dispatch) => {
+        try {
+            console.log("fetch weather data in Celsius started");
+            const result = await axios.get("http://localhost:8080/api/weather/displayInCelsius", { weatherObject });
+            console.log("fetch weather data in Celsius COMPLETED")
+            console.log(result);
+            dispatch(FETCH_WEATHER_C(result));
+        } catch (err) {
+            console.error(err);
+        }
+    };
 }
