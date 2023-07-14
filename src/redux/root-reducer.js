41696127a2faf3295004ec31433aa0fd19bdeb96
Kaifeng <1536771545kai@gmail.com>
2023-07-14 15:37:47 -0400

    add weather reducer to root reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import User from "./users/user.reducer";
+import weatherReducer from "./weather/weather.reducer";
 
 const rootReducer = combineReducers({
   user: User,
+  weather: weatherReducer
 });
 
 export default rootReducer;
