9246df4653f4455b7c0e2e75cfc8d4a7b2ccc7ad
Kaifeng <1536771545kai@gmail.com>
2023-07-18 21:29:19 -0400

    install chart.js

--- src/components/DisplayWeather.jsx
+++ src/components/DisplayWeather.jsx
@@ -1,8 +1,18 @@
-import react ,{useEffect} from "react";
+import react, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {FETCH_WEATHER_F_THUNK,FETCH_WEATHER_C_THUNK} from "../redux/weather/weather.actions"
+import { FETCH_WEATHER_F_THUNK, FETCH_WEATHER_C_THUNK } from "../redux/weather/weather.actions"
 
-function DisplayWeather(){
+function DisplayWeather() {
+    const weather = useSelector((state) => state.weather.tempInFahrenheit);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        const weatherObject = {
+            locationName: "Berlin",
+            date1: "2023-01-01",
+            date2: "2023-07-08"
+        };
+        dispatch(FETCH_WEATHER_F_THUNK(weatherObject));
+    }, []);
     return (
         <div>
             <h1>hi this is weather displays page</h1>
