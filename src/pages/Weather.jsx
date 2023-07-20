49c1f3bc4a14482f35d9feed7e583ed0dc4e34aa
Kaifeng <1536771545kai@gmail.com>
2023-07-20 11:50:55 -0400

    add helper function to change the year of given date to last year

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -3,15 +3,32 @@ import { useDispatch, useSelector } from "react-redux";
 import { FETCH_WEATHER_F_THUNK, FETCH_WEATHER_C_THUNK } from "../redux/weather/weather.actions";
 import DisplayWeather from "../components/DisplayWeather";
 import DisplayRainSnow from "../components/DisplayRainSnow";
+import { useLocation } from "react-router-dom";
 
+// example of accessing to weather page is shown below
+//<Link to = "/weather" state ={{destination:"JFK",startingDate:"2023-09-01",endingDate:"2023-12-30"}}>weather</Link>
 function Weather() {
     let weather = useSelector((state) => state.weather.weatherTemp);
+    const location = useLocation();
+    const {destination,startingDate,endingDate} = location.state;
     const dispatch = useDispatch();
+
+    //Subtracts the year of the given date and returns the date string with the updated year
+    function minusYear(date){
+        const targetDate = new Date(date);
+        const year = targetDate.getFullYear()-1;
+        const month = String(targetDate.getMonth() + 1).padStart(2, '0');
+        const day = String(targetDate.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    }
     useEffect(() => {
         const weatherObject = {
-            locationName: "JFK",
-            date1: "2022-09-01",
-            date2: "2022-12-30"
+            // locationName: "JFK",
+            // date1: minusYear("2023-09-01"),
+            // date2: minusYear("2023-12-30")
+            locationName: destination,
+            date1: minusYear(startingDate),
+            date2: minusYear(endingDate)
         };
         console.log("dispatch: historical weather");
         dispatch(FETCH_WEATHER_F_THUNK(weatherObject));
