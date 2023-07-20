f20c6104a6109f55bce39759501dfbc37fd850f5
Kaifeng <1536771545kai@gmail.com>
2023-07-19 20:24:52 -0400

    add gradient color feature to chart

--- src/components/DisplayWeather.jsx
+++ src/components/DisplayWeather.jsx
@@ -2,9 +2,21 @@ import { Line } from "react-chartjs-2";
 import Chart from "chart.js/auto";
 import { CategoryScale } from "chart.js";
 import { useDispatch, useSelector } from "react-redux";
+import {Bar} from "react-chartjs-2";
 Chart.register(CategoryScale);
 const DisplayWeather = () => {
   const weather = useSelector((state) => state.weather.tempInFahrenheit);
+
+  function visualGradient(chart){
+    const {ctx,chartArea:{top,bottom,left,right}} = chart;
+    const gradiantSegment = ctx.createLinearGradient(0, bottom, 0, top);
+    gradiantSegment.addColorStop(0,'#7cb1b7');
+
+    gradiantSegment.addColorStop(0.5,'#e8c867');
+
+    gradiantSegment.addColorStop(1,'rgb(255, 122, 136)');
+    return gradiantSegment;
+  }
 
   return (
     <div style={{width:700}}>
@@ -18,7 +30,15 @@ const DisplayWeather = () => {
               data: weather.daily.temperature_2m_max.map((data) => data),
               fill: true,
               borderColor: 'rgb(75, 192, 192)',
-              backgroundColor: 'rgba(255, 99, 71, 0.8)',
+              // backgroundColor: 'rgba(255, 99, 71, 0.8)',
+              backgroundColor: (context) =>{
+                const chart = context.chart;
+                const {ctx,chartArea}=chart;
+                if(!chartArea){
+                  return null;
+                }
+                return visualGradient(chart);
+              },
               tension: 0.1
               
             }
