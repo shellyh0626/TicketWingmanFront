ab22581503212239dceb5debbbd3e58f9263a343
9Mohamedismail <9Mohamedismail@gmail.com>
2023-07-20 15:30:35 -0400

    added the logout button and functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 import Profile from "./pages/Profile";
 import EditProfile from "./pages/EditProfile";
 import ProtectedRoute from "./utils/Auth";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import SearchResults from "./components/SearchResults";
 import SearchBar from "./components/SearchBar";
@@ -16,8 +17,22 @@ import { me } from "./redux/users/user.actions";
 import { logout } from "./redux/users/user.actions";
 import Weather from "./pages/Weather";
 
+const LinkButton = ({ to, children, onClick }) => (
+  <NavLink to={to} onClick={onClick}>
+    {children}
+  </NavLink>
+);
+
 function App() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogOut = (event) => {
+    event.preventDefault();
+    dispatch(logout());
+    navigate("/login");
+  };
+
   useEffect(() => {
     const fetchMe = () => {
       dispatch(me());
@@ -27,88 +42,86 @@ function App() {
 
   const isLoggedIn = useSelector((state) => !!state.user.id);
   return (
-    <Router>
-      <div className="App">
-        {/* Navigation */}
-        <nav className="navbar navbar-expand-lg navbar-dark p-3 bg-danger">
-          <ul className="navbar-nav mx-auto">
-            {isLoggedIn ? (
+    <div className="App">
+      {/* Navigation */}
+      <nav className="navbar navbar-expand-lg navbar-dark p-3 bg-danger">
+        <ul className="navbar-nav mx-auto">
+          {isLoggedIn ? (
+            <li className="nav-item">
+              <Link to="/" className="nav-link mx-2 active">
+                Home
+              </Link>
+            </li>
+          ) : (
+            <>
               <li className="nav-item">
                 <Link to="/" className="nav-link mx-2 active">
                   Home
                 </Link>
               </li>
-            ) : (
-              <>
-                <li className="nav-item">
-                  <Link to="/" className="nav-link mx-2 active">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/login" className="nav-link mx-2">
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/signup" className="nav-link mx-2">
-                    Sign up
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/emission" className="nav-link mx-2">
-                    Emission Calculator
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    to="/weather"
-                    state={{
-                      destination: "JFK",
-                      startingDate: "2023-09-01",
-                      endingDate: "2023-12-30",
-                      tempF: true,
-                    }}
-                  >
-                    weather
-                  </Link>
-                </li>
-              </>
-            )}
-            {isLoggedIn && (
               <li className="nav-item">
-                <Link to="/profile" className="nav-link mx-2">
-                  User Profile
+                <Link to="/login" className="nav-link mx-2">
+                  Login
                 </Link>
               </li>
-            )}
-            {isLoggedIn && (
               <li className="nav-item">
-                {/*  <LinkButton className="nav-link mx-2" onClick={handleLogOut}>
-                  Logout
-                </LinkButton> */}
+                <Link to="/signup" className="nav-link mx-2">
+                  Sign up
+                </Link>
               </li>
-            )}
-          </ul>
-        </nav>
+              <li className="nav-item">
+                <Link to="/emission" className="nav-link mx-2">
+                  Emission Calculator
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/weather"
+                  state={{
+                    destination: "JFK",
+                    startingDate: "2023-09-01",
+                    endingDate: "2023-12-30",
+                    tempF: true,
+                  }}
+                >
+                  weather
+                </Link>
+              </li>
+            </>
+          )}
+          {isLoggedIn && (
+            <li className="nav-item">
+              <Link to="/profile" className="nav-link mx-2">
+                User Profile
+              </Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li className="nav-item">
+              <LinkButton className="nav-link mx-2" onClick={handleLogOut}>
+                Logout
+              </LinkButton>
+            </li>
+          )}
+        </ul>
+      </nav>
 
-        {/* Routes */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/profile/*" element={<Profile />} />
-            <Route path="/profile/edit" element={<EditProfile />} />
-          </Route>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/forgot" element={<ForgotPassword />} />
-          <Route exact path="/" element={<SearchBar />} />
-          <Route path="/searchResults" element={<SearchResults />} />
-          <Route path="/weather" element={<Weather />} />
-          <Route path="/emission" element={<Emission />} />
-        </Routes>
-      </div>
-    </Router>
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/profile/*" element={<Profile />} />
+          <Route path="/profile/edit" element={<EditProfile />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot" element={<ForgotPassword />} />
+        <Route exact path="/" element={<SearchBar />} />
+        <Route path="/searchResults" element={<SearchResults />} />
+        <Route path="/weather" element={<Weather />} />
+        <Route path="/emission" element={<Emission />} />
+      </Routes>
+    </div>
   );
 }
 
