11e65e87ca18a55b000041713f0c992727a3e478
Kaifeng <1536771545kai@gmail.com>
2023-07-28 11:51:45 -0400

    add styling to display feature

--- src/components/DisplayPlugs.jsx
+++ src/components/DisplayPlugs.jsx
@@ -1,5 +1,6 @@
 import react, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import "../css/flightsCard.css"
 
 function DisplayPlugs() {
     const foundPlugs = useSelector((state) => state.plugs.plugsType);
@@ -9,25 +10,29 @@ function DisplayPlugs() {
             {/* if foundPlugs is empty then display Loading......, if not empty then display the plugs info of given country */}
             {foundPlugs.length !== 0 ? (
                 <div>
-                    {/* shows the frequency of plugs/socket*/}
-                    <p>Frequency: {foundPlugs[0].frequency.hertz}{foundPlugs[0].frequency.unit}</p>
-                    {/* shows the voltage of plugs/socket*/}
-                    {foundPlugs[0].voltage.volt.map((data, i) => {
-                        return (
-                            <div key={i}>
-                                <p>Voltage: {data}V</p>
-                            </div>
-                        );
-                    })}
                     {/* shows the types of plugs/socket*/}
-                    {foundPlugs[0].plugs.map((data, i) => {
-                        return (
-                            <div key={i}>
-                                <h3>Type: {data.type}</h3>
-                                <img style={{ width: 50, height: 100 }} src={data.img}></img>
-                            </div>
-                        );
-                    })}
+                    <div className="flight-container">
+                        <div>
+                            {/* shows the frequency of plugs/socket*/}
+                            <p>Frequency: {foundPlugs[0].frequency.hertz}{foundPlugs[0].frequency.unit}</p>
+                            {/* shows the voltage of plugs/socket*/}
+                            {foundPlugs[0].voltage.volt.map((data, i) => {
+                                return (
+                                    <div key={i}>
+                                        <p>Voltage: {data}V</p>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                        {foundPlugs[0].plugs.map((data, i) => {
+                            return (
+                                <div key={i} >
+                                    <h3>Type: {data.type}</h3>
+                                    <img style={{ width: 50, height: 100, marginLeft: 10 }} src={data.img}></img>
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
             ) : (
                 <h1>Loading......</h1>
